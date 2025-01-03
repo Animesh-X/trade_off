91454f2f5a8dab79ebefe1ee2ec55df02ea68b3d
Animesh Kumar <120721959+Animesh-X@users.noreply.github.com>
2025-01-03 16:23:11 +0530

    Update title to 'Trade OFF' and improve formatting in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import Navbar from "./myComponent/Navbar";
 
 function RateComponent() {
   return (
-    <div className=" mt-2
-    ">
+    <div
+      className=" mt-2
+    "
+    >
       <p className="text-gray-400 text-sm">
-        <span className="font-semibold text-gray-500">1 SUSHI</span> 
-        <span className="text-gray-500"> ($1.10969) =</span> 
-        <span className="font-semibold text-gray-500"> 0.0005975 WETH</span> 
+        <span className="font-semibold text-gray-500">1 SUSHI</span>
+        <span className="text-gray-500"> ($1.10969) =</span>
+        <span className="font-semibold text-gray-500"> 0.0005975 WETH</span>
         <span className="text-gray-400 "> ($1857.31)</span>
       </p>
     </div>
@@ -21,38 +23,34 @@ function RateComponent() {
 
 function App() {
   return (
-  <>
-  <Navbar />
-    <div className="min-h-screen  flex items-center justify-center px-4 bg-gray-100">
-        
-      <div className="  rounded-lg w-full max-w-md  ">
-        <div className="bg-white  rounded-md p-2">
-
-     
-        <h1 className="text-2xl font-semibold text-gray-800 ">Trade</h1>
-        <RateComponent />
-      
-        </div>
-        <TabbedComponent />
-        <button className="mt-2 w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">
-          <div className="flex justify-center items-center w-full">
-            <ConnectButton />
+    <>
+      <Navbar />
+      <div className="min-h-screen  flex items-center justify-center px-4 bg-gray-100">
+        <div className="  rounded-lg w-full max-w-md  ">
+          <div className="bg-white  rounded-md p-2">
+            <h1 className="text-2xl font-semibold text-gray-800 ">Trade</h1>
+            <RateComponent />
           </div>
-        </button>
+          <TabbedComponent />
+          <div className="mt-2 w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition cursor-pointer">
+            <div className="flex justify-center items-center w-full">
+              <ConnectButton />
+            </div>
+          </div>
+        </div>
       </div>
-    </div></>
+    </>
   );
 }
 
 export default App;
-
 
 // Tabbed component
 function TabbedComponent() {
   const [activeTab, setActiveTab] = useState("Swap");
 
   return (
-    <div >
+    <div>
       <div className="flex border-b mb-1 bg-white  rounded-md ">
         <button
           className={`flex-1 text-center py-2 ${
@@ -75,11 +73,10 @@ function TabbedComponent() {
           Pancake Swap
         </button>
       </div>
-      {activeTab === "Swap" && <SwapTabContent/>}
+      {activeTab === "Swap" && <SwapTabContent />}
       {activeTab === "Limit" && <LimitTabContent />}
     </div>
   );
 }
 
 // Main App
-
